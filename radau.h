17c819b579523699dc307a74a7bee020a2ecbb49
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-10-27 21:13:26 +0200

    radau.h added

---
+++ radau.h
@@ -0,0 +1,15 @@
+#ifndef	_INC_RADAU_H
+#define	_INC_RADAU_H
+
+#define	_STRINGIFY(X)	#X
+#define	STRINGIFY(X)	_STRINGIFY(X)
+
+#define	R_PORT	19162	/* 0x4adau	*/
+#define	R	struct radau *r
+
+struct radau
+  {
+  };
+
+#endif
+
