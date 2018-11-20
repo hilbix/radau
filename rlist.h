d906d210034e258f13803de669f1d055dac51bfa
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-11-20 08:47:15 +0100

    rlist.h added

---
+++ rlist.h
@@ -0,0 +1,33 @@
+/* List management	*/
+
+#define	RLIST_ADD(p, head)		\
+        (p)->last	= 0;		\
+        (p)->next	= (head);	\
+        if ((p)->next)			\
+          {				\
+            FATAL((p)->next->last);	\
+            (p)->next->last	= (p);	\
+          }				\
+        (head)	= (p);			\
+
+#define	RLIST_DEL(p, head)		\
+        if ((head) == (p))		\
+          (head) = (p)->next;		\
+        if ((p)->next)			\
+          (p)->next->last = (p)->last;	\
+        (p)->next	= 0;		\
+        (p)->last	= 0;		\
+
+typedef struct relem	*R_ELEM;
+
+struct rlist
+  {
+    R_ELEM	head;
+  };
+
+struct relem
+  {
+    R_ELEM	next, last;
+    void	*data;
+  };
+
