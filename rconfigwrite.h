e93612939233c78eeaa10ba42c5f661c0c572db2
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-11-23 02:24:18 +0100

    rconfigwrite.h added

---
+++ rconfigwrite.h
@@ -0,0 +1,12 @@
+/* Config writer
+ *
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ */
+
+static void
+r_config_write_do(R, FILE *fd, const char *name)
+{
+  000;
+}
+
