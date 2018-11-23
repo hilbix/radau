11419b4b1cdc9cc359c2d1d1e3627cdb51e334f4
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-11-23 02:21:09 +0100

    rcon.h modified

--- rcon.h
+++ rcon.h
@@ -1,13 +1,25 @@
-/* basic IO	*/
+/* basic IO (console)
+ *
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ */
 
-#include "radau.h"
+#if	RADAU_PHASE==1
+
+#if 0
+void (*out)(R, const char *what, const char *msg, va_list list, int e);
+#endif
+void (*warn)(R, const char *msg, ...);
+void (*info)(R, const char *msg, ...);
+
+#elif	RADAU_PHASE==2
 
 #include <stdlib.h>
 #include <stdarg.h>
 #include <errno.h>
 
 static void
-vOUT(R, const char *what, const char *msg, va_list list, int e)
+r_con_vout(R, const char *what, const char *msg, va_list list, int e)
 {
   fprintf(stderr, "%s: ", what);
   vfprintf(stderr, msg, list);
@@ -17,12 +29,40 @@ vOUT(R, const char *what, const char *msg, va_list list, int e)
 }
 
 static void
-WARN(R, const char *msg, ...)
+r_con_warn(R, const char *msg, ...)
 {
   va_list	list;
 
   va_start(list, msg);
-  vOUT(r, "WARN", msg, list, 0);
+  r_con_vout(r, "WARN", msg, list, 0);
   va_end(list);
 }
+
+static void
+r_con_info(R, const char *msg, ...)
+{
+  va_list	list;
+
+  va_start(list, msg);
+  r_con_vout(r, "INFO", msg, list, 0);
+  va_end(list);
+}
+
+static void
+r_con_init(R)
+{
+}
+
+static void
+r_con_exit(R)
+{
+}
+
+#elif	RADAU_PHASE==3
+
+r->warn	= r_con_warn;
+r->info	= r_con_info;
+r->modadd(r, r_con_init, r_con_exit);
+
+#endif
 
