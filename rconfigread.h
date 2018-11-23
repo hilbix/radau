8ad318f73e0cb30fbdd4bdfce07b83b8bac5bd0b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-11-23 02:24:18 +0100

    rconfigread.h added

---
+++ rconfigread.h
@@ -0,0 +1,12 @@
+/* Config reader
+ *
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ */
+
+static void
+r_config_read_do(R, FILE *fd, const char *name)
+{
+  000;
+}
+
