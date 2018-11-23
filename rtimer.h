2bb7b3ca7210cbe0a9bef50e22a3aef4ae9bee93
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-11-23 02:21:09 +0100

    rtimer.h modified

--- rtimer.h
+++ rtimer.h
@@ -1,27 +1,36 @@
-/* Timing	*/
+/* Timing Support
+ *
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ */
+
+#if	RADAU_PHASE==1
+
+int	tick;
+long	delay;
+
+#elif	RADAU_PHASE==2
 
 #include <signal.h>
 #include <sys/time.h>
 
-static struct radau *_r;
+static struct radau *_r_timer;
 
 static void
 r_timer(int x)
 {
-  if (!_r) return;
-  _r->tick++;
+  if (!_r_timer) return;
+  _r_timer->tick++;
 }
 
-void
-r_timer_start(R)
+static void
+r_timer_set(R, long usec)
 {
   struct timeval	tv;
   struct itimerval	t;
 
-  _r		= r;
-
-  tv.tv_sec	= 1;
-  tv.tv_usec	= 0;
+  tv.tv_sec	= usec/1000000;
+  tv.tv_usec	= usec%1000000;
 
   t.it_value	= tv;
   t.it_interval	= tv;
@@ -29,4 +38,26 @@ r_timer_start(R)
   FATAL(signal(SIGALRM, r_timer)==SIG_ERR);
   FATAL(setitimer(ITIMER_REAL, &t, NULL));
 }
+
+static void
+r_timer_init(R)
+{
+  FATAL(_r_timer!=r);
+  r_timer_set(r, r->delay);
+}
+
+static void
+r_timer_exit(R)
+{
+  FATAL(_r_timer!=r);
+  r_timer_set(r, 0l);
+}
+
+#elif	RADAU_PHASE==3
+
+_r_timer	= r;
+r->delay	= 1000000l;
+r->modadd(r, r_timer_init, r_timer_exit);
+
+#endif
 
